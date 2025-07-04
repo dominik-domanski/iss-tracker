e613cb485827a44df773a57afb960fa75f9f6214
domanski-bwr <dominik.do@bw-robotics.com>
2025-07-04 12:19:54 +0200

    scripts added

---
+++ scripts/run-ws.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "Discarding local changes and untracked files..."
+git reset --hard
+git clean -fd
+
+echo "Switching to 'websocket' branch..."
+git checkout websocket
+
+echo "Removing all node_modules and lock files..."
+rm -rf node_modules yarn.lock
+rm -rf client/node_modules client/yarn.lock
+rm -rf server/node_modules server/yarn.lock
+
+echo "Installing dependencies (root + client + server)..."
+yarn bootstrap
+
+echo "Running app in DEV mode..."
+yarn dev
